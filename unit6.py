56ce9c799d6a7e96c6b1177b814966d231178f89
Dr. (Prince) Mighty Itauma Itauma <609418+amightyo@users.noreply.github.com>
2020-03-17 15:22:07 -0400

    add string of skating jumps

---
+++ unit6.py
@@ -0,0 +1,19 @@
+# Write a Python program that does the following.
+
+# Create a string that is a long series of words separated by spaces. The string is your own creative choice.
+# It can be names, favorite foods, animals, anything. Just make it up yourself. Do not copy the string from another source.
+
+skating_jumps = 'waltz salchow half-loop toe-loop flip loop lutz split axel'
+
+# Turn the string into a list of words using split.
+skating_jumps_list = skating_jumps.split()
+
+# Delete three words from the list, but delete each one using a different kind of Python operation.
+
+# Sort the list.
+
+# Add new words to the list (three or more) using three different kinds of Python operation.
+
+# Turn the list of words back into a single string using join.
+
+# Print the string.
