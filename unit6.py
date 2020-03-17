d58a05f782b668078c083201b6b0254542d6a32a
Dr. (Prince) Mighty Itauma Itauma <609418+amightyo@users.noreply.github.com>
2020-03-17 15:43:12 -0400

    sort list

--- unit6.py
+++ unit6.py
@@ -7,11 +7,17 @@ skating_jumps = 'waltz salchow half-loop toe-loop flip loop lutz split axel'
 
 # Turn the string into a list of words using split.
 skating_jumps_list = skating_jumps.split()
+print(skating_jumps_list)
 
 # Delete three words from the list, but delete each one using a different kind of Python operation.
+skating_jumps_list.pop(2)
+del skating_jumps_list[5]
+skating_jumps_list.remove('axel')
+print(skating_jumps_list)
 
 # Sort the list.
-
+skating_jumps_list.sort()
+print(skating_jumps_list)
 # Add new words to the list (three or more) using three different kinds of Python operation.
 
 # Turn the list of words back into a single string using join.
